2ee2cb3015dca1c9ad2a51aeea2d3093a8bc4e22
Arthur Immich <arthurimmichof@gmail.com>
2021-04-21 00:20:37 -0300

    finished crud

--- lib/database.dart
+++ lib/database.dart
@@ -13,35 +13,41 @@ class DatabaseConnection {
     );
   }
 
-  Future create(Contato contato) async {
-    try {
-      _getDatabase().then((db) => db.insert('contatos', contato.toMap()));
-      // final Database db = await _getDatabase();
-      // await db.insert('contatos', contato.toMap());
-    } catch (e) {
-      print(e);
-      return;
-    }
-  }
+  Future create(Contato contato) async => _getDatabase()
+          .then((db) => db.insert('contacts', contato.toMap()))
+          .catchError((e) {
+        print(e);
+      });
+
+  Future delete(int id) async => _getDatabase()
+          .then((db) => db.delete('contacts', where: "id = ?", whereArgs: [id]))
+          .catchError((e) {
+        print(e);
+      });
+
+  Future upate(Contato contato) async => _getDatabase()
+          .then((db) => db.update('contacts', contato.toMap(),
+              where: "id = ?", whereArgs: [contato.id]))
+          .catchError((e) {
+        print(e);
+      });
 
   Future<List<Contato>> getContacts() async {
-    try {
-      List<Map<String, dynamic>> maps =
-          await _getDatabase().then((db) => db.query('contatos'));
-      return List.generate(
+    return _getDatabase().then((db) {
+      return db.query('contacts').then((List<Map<String, dynamic>> maps) {
+        return List.generate(
           maps.length,
           (i) => Contato(
-                id: maps[i]['id'],
-                name: maps[i]['name'],
-                phone: maps[i]['phone'],
-                email: maps[i]['email'],
-                instagram: maps[i]['instagram'],
-                facebook: maps[i]['facebook'],
-                linkedin: maps[i]['linkedin'],
-              ));
-    } catch (e) {
-      print(e);
-      // return;
-    }
+            id: maps[i]['id'],
+            name: maps[i]['name'],
+            phone: maps[i]['phone'],
+            email: maps[i]['email'],
+            instagram: maps[i]['instagram'],
+            facebook: maps[i]['facebook'],
+            linkedin: maps[i]['linkedin'],
+          ),
+        );
+      });
+    });
   }
 }
